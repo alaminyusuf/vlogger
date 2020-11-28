000a9fb33dc018526530082beb169b0e51570bfa
dev-empire <alaminyusuf131@gmail.com>
2020-11-28 20:01:02 +0100

    mae navbar dynamic

--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -1,20 +1,39 @@
 import React from 'react';
 import NextLink from 'next/link';
-import { Box, Flex, Link } from '@chakra-ui/react';
+import { Box, Button, Flex, Link } from '@chakra-ui/react';
+import { useMeQuery } from '../generated/graphql';
 
 interface navbarProp {}
 
 const Navbar: React.FC<navbarProp> = ({}) => {
-  return (
-    <Flex bg='tomato' py={4}>
-      <Box ml='auto'>
+  const [{ data, fetching }] = useMeQuery();
+
+  let body = null;
+
+  if (fetching) {
+    //
+  } else if (!data?.me) {
+    body = (
+      <>
         <NextLink href='/login'>
           <Link mr={2}>Login</Link>
         </NextLink>
         <NextLink href='register'>
           <Link mr={2}>Register</Link>
         </NextLink>
-      </Box>
+      </>
+    );
+  } else {
+    body = (
+      <Flex mr={2}>
+        <Box mr={2}>{data.me.username}</Box>
+        <Button variant='link'>LogOut</Button>
+      </Flex>
+    );
+  }
+  return (
+    <Flex bg='tomato' py={4}>
+      <Box ml='auto'>{body}</Box>
     </Flex>
   );
 };
