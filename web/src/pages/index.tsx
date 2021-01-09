9d5a347d15d39c12f635781f67361c5cd38a2fb9
dev-empire <alaminyusuf131@gmail.com>
2021-01-09 22:32:47 +0100

    set queried post to be displayed as stacks

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import { withUrqlClient } from 'next-urql';
-import { Link } from '@chakra-ui/react';
+import { Link, Stack, Box, Heading, Text } from '@chakra-ui/react';
 import { createUrqlClient } from '../utils/createUrqlClient';
 import { usePostsQuery } from '../generated/graphql';
 import { Layout } from '../components/Layout';
@@ -8,7 +8,11 @@ import { Layout } from '../components/Layout';
 import NextLink from 'next/link';
 
 const Index = () => {
-  const [{ data }] = usePostsQuery();
+  const [{ data }] = usePostsQuery({
+    variables: {
+      limit: 10,
+    },
+  });
 
   return (
     <>
@@ -23,9 +27,14 @@ const Index = () => {
         {!data ? (
           <div>Loading...</div>
         ) : (
-          data.posts.map((post) => {
-            return <div key={post.id}>{post.title}</div>;
-          })
+          <Stack spacing={8}>
+            {data.posts.map((post) => (
+              <Box key={post.id} p={5} shadow='md' borderWidth='1px'>
+                <Heading fontSize='xl'>{post.title}</Heading>
+                <Text mt={4}>{post.content}</Text>
+              </Box>
+            ))}
+          </Stack>
         )}
       </Layout>
     </>
