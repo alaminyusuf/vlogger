d74522a4562c7e3618c5372c5fde3ac142342eb4
Al-amin Yusuf <alaminyusuf131@gmail.com>
2021-04-23 12:01:38 +0100

    made navbar tally with profile page

--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -3,49 +3,65 @@ import NextLink from 'next/link';
 import { Box, Button, Flex, Link } from '@chakra-ui/react';
 import { useLogoutMutation, useMeQuery } from '../generated/graphql';
 import { isServer } from '../utils/isServer';
+import { useRouter } from 'next/router';
 
 interface navbarProp {}
 
 const Navbar: React.FC<navbarProp> = ({}) => {
-  const [{ fetching: logoutFetching }, logout] = useLogoutMutation();
-  const [{ data, fetching }] = useMeQuery({
-    pause: isServer(),
-  });
+	const [{ fetching: logoutFetching }, logout] = useLogoutMutation();
+	const [{ data, fetching }] = useMeQuery({
+		pause: isServer(),
+	});
+	const router = useRouter();
 
-  let body = null;
+	let body = null;
 
-  if (fetching) {
-    //
-  } else if (!data?.me) {
-    body = (
-      <>
-        <NextLink href='/login'>
-          <Link mr={2}>Login</Link>
-        </NextLink>
-        <NextLink href='register'>
-          <Link mr={2}>Register</Link>
-        </NextLink>
-      </>
-    );
-  } else {
-    body = (
-      <Flex mr={2}>
-        <Box mr={2}>{data.me.username}</Box>
-        <Button
-          variant='link'
-          onClick={() => logout()}
-          isLoading={logoutFetching}
-        >
-          LogOut
-        </Button>
-      </Flex>
-    );
-  }
-  return (
-    <Flex bg='tan' py={5}>
-      <Box ml='auto'>{body}</Box>
-    </Flex>
-  );
+	if (fetching) {
+		//
+	} else if (!data?.me) {
+		body = (
+			<>
+				<NextLink href='/login'>
+					<Link mr={2}>Login</Link>
+				</NextLink>
+				<NextLink href='register'>
+					<Link mr={2}>Register</Link>
+				</NextLink>
+			</>
+		);
+	} else if (router.pathname === '/profile') {
+		body = (
+			<Flex mr={5}>
+				<Button
+					variant='link'
+					onClick={() => logout()}
+					isLoading={logoutFetching}
+				>
+					LogOut
+				</Button>
+			</Flex>
+		);
+	} else {
+		body = (
+			<Flex mr={2}>
+				<Link href='profile'>
+					<Box mr={2}>{data.me.username}</Box>
+				</Link>
+				<Button
+					variant='link'
+					onClick={() => logout()}
+					isLoading={logoutFetching}
+				>
+					LogOut
+				</Button>
+			</Flex>
+		);
+	}
+	return (
+		<Flex bg='tan' py={5}>
+			<Box ml='auto'>{body}</Box>
+		</Flex>
+	);
 };
 
 export default Navbar;
