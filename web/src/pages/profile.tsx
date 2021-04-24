0ecd22e582a12883fc6d773887c4a5a000e14cd2
Al-amin Yusuf <alaminyusuf131@gmail.com>
2021-04-24 20:08:28 +0100

    profile page

--- web/src/pages/profile.tsx
+++ web/src/pages/profile.tsx
@@ -3,7 +3,7 @@ import { isServer } from '../utils/isServer';
 import { Layout } from '../components/Layout';
 import { useMeQuery } from '../generated/graphql';
 import { withUrqlClient } from 'next-urql';
-import { Heading } from '@chakra-ui/react';
+import { Flex, Heading, Link } from '@chakra-ui/react';
 import { useIsAuth } from '../utils/useIsAuth';
 
 const profilePage = () => {
@@ -17,6 +17,14 @@ const profilePage = () => {
 			<Heading textTransform='capitalize' fontWeight='400'>
 				{data?.me?.username}
 			</Heading>
+			<Flex>
+				<Heading fontSize={18} my={4} fontWeight='400'>
+					Posts
+				</Heading>
+				<Link href='create-post' ml='auto' my={4} fontSize={14}>
+					Create Post
+				</Link>
+			</Flex>
 		</Layout>
 	);
 };
