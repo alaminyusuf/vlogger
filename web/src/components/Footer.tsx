65c41c77d1b983854f197696f4e5832c62bc8254
Al-amin Yusuf <alaminyusuf131@gmail.com>
2021-04-24 10:43:07 +0100

    footer componnent

---
+++ web/src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import { Flex, Box } from '@chakra-ui/react';
+const Footer = () => {
+	return (
+		<footer>
+			<Flex justifyContent='space-evenly' py={5} bgColor='tan' mt={5}>
+				<Box>Fashionista</Box>
+				&copy;2021
+			</Flex>
+		</footer>
+	);
+};
+
+export default Footer;
