fff39f64e6aed3bdcf3ba252b095eed4d0787ccf
Al-amin Yusuf <alaminyusuf131@gmail.com>
2021-04-27 13:48:37 +0100

    commit chnajes

--- web/src/components/Footer.tsx
+++ web/src/components/Footer.tsx
@@ -2,7 +2,13 @@ import { Flex, Box } from '@chakra-ui/react';
 const Footer = () => {
 	return (
 		<footer>
-			<Flex justifyContent='space-evenly' py={5} bgColor='tan' mt={5}>
+			<Flex
+				justifyContent='space-evenly'
+				py={5}
+				bgColor='tan'
+				mt={5}
+				bottom={0}
+			>
 				<Box>Fashionista</Box>
 				&copy;2021
 			</Flex>
--- web/src/components/Layout.tsx
+++ web/src/components/Layout.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Wrapper, WrapperVariant } from './Container';
+import Footer from './Footer';
 import NavBar from './Navbar';
 
 interface LayoutProps {
-  variant?: WrapperVariant;
+	variant?: WrapperVariant;
 }
 
 export const Layout: React.FC<LayoutProps> = ({ children, variant }) => {
-  return (
-    <>
-      <NavBar />
-      <Wrapper variant={variant}>{children}</Wrapper>
-    </>
-  );
+	return (
+		<>
+			<NavBar />
+			<Wrapper variant={variant}>{children}</Wrapper>
+			<Footer />
+		</>
+	);
 };
