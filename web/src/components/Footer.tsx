bb93bce684080547e0d711ee99a95e73f8e2c33b
Al-amin Yusuf <alaminyusuf131@gmail.com>
2021-04-28 08:28:56 +0100

    commit changes

--- web/src/components/Footer.tsx
+++ web/src/components/Footer.tsx
@@ -1,18 +1,27 @@
 import { Flex, Box } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
+import { usePostsQuery } from '../generated/graphql';
 const Footer = () => {
 	const router = useRouter();
+	const variables = {
+		limit: 15,
+		cursor: null as null | string,
+	};
+	const [{ data }] = usePostsQuery({
+		variables,
+	});
 	let body = null;
 
-	if (router.pathname === '/') {
+	if (router.pathname !== '/' || data!.posts.posts.length <= 2) {
 		body = (
 			<Flex
 				justifyContent='space-evenly'
 				py={5}
 				bgColor='tan'
 				mt={5}
-				position='sticky'
+				position='fixed'
 				w='100%'
+				bottom={0}
 			>
 				<Box>Fashionista</Box>
 				&copy;2021
@@ -25,8 +34,7 @@ const Footer = () => {
 				py={5}
 				bgColor='tan'
 				mt={5}
-				bottom={0}
-				position='fixed'
+				position='sticky'
 				w='100%'
 			>
 				<Box>Fashionista</Box>
